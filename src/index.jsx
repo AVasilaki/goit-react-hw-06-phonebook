0fb2f8eda470b337d48af09b39f1cb5adb0a7dc9
AVasilaki <poseydon463@gmail.com>
2023-12-04 12:36:00 +0200

    upd

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import './index.css';
-import { store } from './redux/store';
+import store from './redux/store';
+import { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-// import { contactsReducer, filterReducer } from './reducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-// const initialState = {
-//   contacts: [
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ],
-//   filter: '',
-// };
-// const rootReducer = (state = initialState, action) => {
-//   return state;
-// };
-
-// const enhancer = devToolsEnhancer();
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
 });
-// console.log(store, 'store');
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['filter'],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  // {
+  //   contacts: contactsReducer,
+  //   filter: filterReducer,
+  // },
+});
+// console.log(store, persistReducer, persistConfig, persistStore, 'store');
+export const persistor = persistStore(store);
+export default store;
